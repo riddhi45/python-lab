15a81e2b7cfe70108206e35f7e8b714d93ae0519
riddhi45 <92423633+riddhi45@users.noreply.github.com>
2021-11-29 23:54:33 +0530

    Add files via upload

---
+++ 47.py
@@ -0,0 +1,11 @@
+def sum_digits_string(str1):
+    sum_digit = 0
+    for x in str1:
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+
+    return sum_digit
+     
+print(sum_digits_string("Acropolis596CT"))
+print(sum_digits_string("CS35IT"))
